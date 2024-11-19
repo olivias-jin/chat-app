368212739cab0bfe6062861da869dc30778fb0e7
jsy5888 <oliviasunjin0318@gmail.com>
2024-11-19 02:15:17 -0500

    updated

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,72 @@
-import React, { useEffect} from 'react';
-import { StyleSheet, View, Text, BackHandler } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, BackHandler, KeyboardAvoidingView, ImageBackground } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
 
 
 const Chat = ({ route, navigation }) => {
-    const { name } = route.params;
-    const { color } = route.params;
+    const [messages, setMessages] = useState({});
+    const { name, color } = route.params;
+
+
 
     useEffect(() => {
         navigation.setOptions({ title: name });
-        
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Data(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any"
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Data(),
+                system: true,
+            },
+        ]);
     }, []);
 
+    const onSend = (newMessages) => {
+        setMessages(previousMessages =>
+            GiftedChat.append(previousMessages, newMessages)
+        );
+    };
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: "#000"
+                    },
+                    left: {
+                        backgroundColor: "#FFF"
+                    }
+                }}
+            />
+        );
+    };
+
     return (
-        <View style={[styles.container, {backgroundColor: color}]}>
-            <Text></Text>
+        <View style={{ flex: 1, backgroundColor: color }}>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+            />
+            {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
